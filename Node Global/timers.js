25a4ef3a8c952035e6041bc0f569830f40f9f674
sklohani <s.shobhit9901@gmail.com>
2020-06-14 19:47:50 +0530

    Node Global Complete

--- Node Global/timers.js
+++ Node Global/timers.js
@@ -1,15 +1,20 @@
-const waitTime = 3000;
+const waitTime = 5000;
 const waitInterval = 500;
 let currentTime = 0;
 
 const incTime = () => {
     currentTime += waitInterval;
-    console.log(`Waiting ${currentTime/1000} seconds.`);
+    const pt = Math.floor((currentTime/waitTime)*100);
+    process.stdout.clearLine(); //
+    process.stdout.cursorTo(0); //
+    process.stdout.write(`Waiting ... ${pt}%`);
 }
 console.log(`Setting a ${waitTime/1000} second delay!`);
 
 const timer = () => {
     clearInterval(interval);
+    process.stdout.clearLine();
+    process.stdout.cursorTo(0);
     console.log("Done");
 }
 
